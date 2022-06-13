d4c5c9ed5a6e5c5cafa152212d143b920956ccd8
BigBirdGamer <87754100+BigBirdGamer@users.noreply.github.com>
2022-06-13 16:21:54 +0800

    added static file

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express")
 const app = express()
 const port = process.env.PORT || 3000;
 
+//?
+app.use(express.static("./frontend/dist"))
+
 app.get("/", (req,res)=>{
     res.send("Test Lucas holidays")
 })
